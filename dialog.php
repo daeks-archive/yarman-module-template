979e72ec6268937498845020be279d565534839a
daeks <daeks@gmx.de>
2017-03-08 08:30:41 +0100

    added dialog error

--- dialog.php
+++ dialog.php
@@ -5,6 +5,7 @@ require_once(dirname(realpath(__FILE__)).DIRECTORY_SEPARATOR.'config.php');
 if (network::get('action') != '') {
   switch (network::get('action')) {
     default:
+      network::error('invalid action - '.network::get('action'));
       break;
   }
 }
